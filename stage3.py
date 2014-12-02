d4ad7be70ab2bef6824a14d76c62526d1dc6f81e
zaynaib <zaynaibg@gmail.com>
2014-12-02 13:07:33 -0600

    Update stage3.py

--- stage3.py
+++ stage3.py
@@ -6,6 +6,10 @@ In this challenge, the API is going to give you another dictionary. The first va
 The second value, array, is an array of strings. Your job is to return an array containing only the 
 strings that do not start with that prefix.
 
+Same methodology as stage II. 
+When I uploaded the values given to me by the api python reads the values as unicode. In order to use the method starts with
+I had to change all of preList values into strings. I did another loop in order to remove the word that had the prefix. But the if
+statment executes one time. In order to prompt the program to continue search for other words that had the prefix I used the keyword continue.
 '''
 
 import requests
@@ -20,8 +24,11 @@ preList = values["result"].get("array")
 preWord = str(values["result"].get("prefix"))
 
 
+
+
 for i in range (0, len(preList)):
     preList[i] = str(preList[i])
+    
 
 for word in preList:
     if word.startswith(preWord):
