85b1af5dea5ca1cadaceaf1fd9dbfde1e91ee5c9
zaynaib <zaynaibg@gmail.com>
2014-12-02 12:51:35 -0600

    Update stage1.py

--- stage1.py
+++ stage1.py
@@ -10,8 +10,8 @@ data = '{"token": "24DVHp6MNU"}'
 t=requests.post(url, data=data)
 word = json.loads(t.text)
 theword = str(word["result"])
-reverseWord = theword[::-1]
-key1={"string": reverseWord, "token":"24DVHp6MNU"}
-key=json.dumps(key1)
-reversePost2 = requests.post('http://challenge.code2040.org/api/validatestring ', data=key)
-reversePost2.text
+reverseWord = theword[::-1] #Extended Slice with a stepwise of -1 so it will copy the list in reverse order
+key1={"string": reverseWord, "token":"24DVHp6MNU"} #create a dictionary
+key=json.dumps(key1) #turn the dictionary to a json object
+reversePost2 = requests.post('http://challenge.code2040.org/api/validatestring ', data=key) #send the information to the api
+reversePost2.text 
