2d1f5c67b3b3f86e4d9c9a438fe3f0e16198efec
zaynaib <zaynaibg@gmail.com>
2014-12-03 13:59:16 -0600

    Update stage4.py

--- stage4.py
+++ stage4.py
@@ -5,29 +5,34 @@ Stage IV: The dating game
 The API will again give you a dictionary. The value for datestamp is a string, formatted as an ISO 8601 datestamp.
 The value for interval is a number of seconds.
 
-This challenge was very tricky for me. I have rarely experimented with dates in programming. This is my first time doing it in
-python. I retreive the values turn them into strings. For datestamp I converted to seconds in order to make it easier for me
-to add time. I cast datestamp and interval information as intergers in order to add them.  Changed the seconds into
-at datestamp and tried to formatit in isoformat. I tried figuring out formatting the mircoseconds. Still working on it.
+This challenge was very tricky for me. I have rarely experimented with dates in programming. I refered to this python wiki article on working with time: https://wiki.python.org/moin/WorkingWithTime. I retreive the values turn them into strings. For datestamp I converted to seconds in order to make it easier for me
+I cast d.atestamp and interval information as intergers in order to add them.  Next I changed the seconds into isoformat.
 '''
-
-import dateutil.parser as dp
-import datetime
 import requests
 import json
+from datetime import datetime
+from datetime import datetime, timedelta
+
 url = 'http://challenge.code2040.org/api/time'
 data = '{"token": "24DVHp6MNU"}'
 timeRequests=requests.post(url, data=data)
 values = json.loads(timeRequests.text)
 datestamp= str( values["result"].get("datestamp") )
 interval =  str(values["result"].get("interval"))
-t = datestamp
-parsed_t = dp.parse(t)
-t_in_seconds = parsed_t.strftime('%s')
-t_in_seconds
-totalTime = int(t_in_seconds )+ int(interval)
-myTime = datetime.datetime.fromtimestamp(totalTime).isoformat()
-key1={"datestamp": myTime, "token":"24DVHp6MNU"}
+
+# strptime() parses a string representing a time according to a format
+utc_dt = datetime.strptime(datestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+# convert UTC datetime to seconds since the Epoch -point where time starts 1/1/1970
+
+timestamp = (utc_dt - datetime(1970, 1, 1)).total_seconds()
+
+
+
+timestamp1= float(timestamp) + float(interval)
+dt1 = datetime(1970, 1, 1) + timedelta(seconds=timestamp1)
+
+finalT =dt1.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+key1={"datestamp": finalT, "token":"24DVHp6MNU"}
 key=json.dumps(key1)
 reversePost2 = requests.post('http://challenge.code2040.org/api/validatetime', data=key)
 reversePost2.text	
